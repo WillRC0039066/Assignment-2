74f54ecfef85a5ad680c1222107ad97cee6485d3
William Malkin <rc0039066@student.riversidecollege.ac.uk>
2019-10-14 14:52:39 +0100

    iufdug

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,45 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            double PR1, PR2, PR3;
+            // shows when each text box is being used in each set of code// 
+
+            try
+            {
+                PR1 = double.Parse(textBox7.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invaled Input");
+                PR1 = 0.0;
+                // if input given is not a number then it will be seen as an error//
+                            
+            }
+            try
+            {
+                PR2 = double.Parse(textBox5.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invaled Input");
+                PR2 = 0.0;
+                // if input given is not a number then it will be seen as an error//
+
+            }
+
+
+
+
+
+
+        }
     }
 }
